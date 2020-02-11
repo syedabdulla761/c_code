3f929dd4fa5be3a9f855d9e81d65868e8827c112
syedabdulla761 <60843280+syedabdulla761@users.noreply.github.com>
2020-02-11 19:25:53 +0530

    Add files via upload

---
+++ ASCII_Value.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char chr;
+    printf("enter a character:");
+    scanf("%c",&chr);
+    printf("you entered %c.\n",chr);
+    printf("ASCII value is %d.",chr);
+    return 0;
+}   
---
+++ incdec.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=2,b=6;
+    float c=3.6;
+    printf("++a=%d\n",++a);
+    printf("--b=%d\n",--b);
+    printf("++c=%f\n",++c);
+    return 0;
+}
