a934b9f1facc265bd8c0229afac55a9578d6b7d7
syedabdulla761 <syedabdulla761@gmail.com>
2020-03-02 19:16:41 +0530

    adding modified file

--- fibonacci.c
+++ fibonacci.c
@@ -2,12 +2,12 @@
 int main()
 {
     int i,a,b=0,c=1,d;
-    printf("Enter a number :");
+    printf("Enter a number:");
     scanf("%d",&a);
-    for(i=0;i<a;++i)
+    for(i=1;i<=a;++i)
     {
-        d=b+c;
         printf("%d\n",b);
+        d=b+c;
         b=c;
         c=d;
     }
