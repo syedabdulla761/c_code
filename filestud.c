f1b404e95d38f1a13a0969a8f817dd337699970d
syedabdulla761 <syedabdulla761@gmail.com>
2020-12-20 22:27:43 +0530

    the file i have opened inside this program has other name

---
+++ filestud.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+struct student
+{
+    int regno;
+    char name[20];
+};
+void main()
+{
+    struct student stud[20],temp;
+    FILE *fout,*fin;
+    int i,n;
+
+    fout=fopen("student.dat","w");
+    if(fout==NULL)
+    {
+    printf("Error, could not open file....");
+    exit(0);
+    }
+    printf("Enter number of students:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("For student %d:\n",i+1);
+        printf("Name:");
+        scanf("%s",stud[i].name);
+        printf("Register no:");
+        scanf("%d",&stud[i].regno);
+    }
+    fwrite(stud,sizeof(struct student),n,fout);
+    fclose(fout);
+    fin=fopen("student.dat","r");
+    if(fin==NULL)
+    {
+        printf("Error, could not read from file....");
+        exit(0);
+    }
+    printf("\nData read from file is as follows:\n");
+    while(fread(&temp,sizeof(struct student),1,fin))
+    {
+        printf("Name : %s\n",temp.name);
+        printf("Reg No : %d\n\n",temp.regno);
+    }
+    fclose(fin);
+}
